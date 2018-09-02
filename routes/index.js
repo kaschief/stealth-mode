e5a63f7e21187bfd2f9685e05d68b838676f04b0
Florian Müller <florianmueller@Florians-MacBook-Pro-2.local>
2018-09-02 12:22:53 +0200

    GetRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
+});
+
+// GET SIGNUP, Logout and LOGIN Pages
+router.get('/login', (req, res, next) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+});
+
+router.get('/logout', (req, res, next) => {
+    res.render('logout');
+});
+
+//GET MyList and Favourites, Careers and About
+
+router.get('/mylist', (req, res, next) => {
+    res.render('mylist');
+});
+
+router.get('/favorites', (req, res, next) => {
+    res.render('favorites');
+});
+
+router.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+router.get('/careers', (req, res, next) => {
+    res.render('careers');
 });
 
 module.exports = router;

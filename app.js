21716114c36739c77daf0b29c8f18eb7f021d5dc
kaschief <kaschiefj@gmail.com>
2018-09-05 15:44:19 +0200

    logo added, red links

--- app.js
+++ app.js
@@ -1,92 +1,96 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/User');
-const flash = require('connect-flash');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/User");
+const flash = require("connect-flash");
 const app = express();
 
 mongoose
-    .connect(
-        'mongodb://localhost/stealth-mode',
-        { useNewUrlParser: true }
-    )
-    .then(x => {
-        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-    })
-    .catch(err => {
-        console.error('Error connecting to mongo', err);
-    });
+  .connect(
+    "mongodb://localhost/stealth-mode",
+    { useNewUrlParser: true }
+  )
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Require Session Middleware
 app.use(
-    session({
-        secret: 'our-passport-local-strategy-app',
-        resave: true,
-        saveUninitialized: true
-    })
+  session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true
+  })
 );
 
-app.use(session({ cookie: { maxAge: 60 * 60 * 1000 } }));
+app.use(session({ cookie: { maxAge: 60 * 60 * 10000 } }));
 
 //Serialize Sessions
 passport.serializeUser((user, cb) => {
-    cb(null, user._id);
+  cb(null, user._id);
 });
 
 passport.deserializeUser((id, cb) => {
-    User.findById(id, (err, user) => {
-        if (err) {
-            return cb(err);
-        }
-        cb(null, user);
-    });
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
 });
 
 app.use(flash());
 
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField: 'email'
-        },
-        (email, password, next) => {
-            User.findOne({ email }, (err, user) => {
-                if (err) {
-                    return next(err);
-                }
-                if (!user) {
-                    return next(null, false, {
-                        message: 'Sorry, the email address was incorrect'
-                    });
-                }
-                if (!bcrypt.compareSync(password, user.password)) {
-                    return next(null, false, { message: 'The password was incorrect' });
-                }
-
-                return next(null, user);
-            });
+  new LocalStrategy(
+    {
+      usernameField: "email"
+    },
+    (email, password, next) => {
+      User.findOne({ email }, (err, user) => {
+        if (err) {
+          return next(err);
         }
-    )
+        if (!user) {
+          return next(null, false, {
+            message: "Sorry, the email address was incorrect"
+          });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return next(null, false, { message: "The password was incorrect" });
+        }
+
+        return next(null, user);
+      });
+    }
+  )
 );
 
 // Initialize Sessions
@@ -95,30 +99,22 @@ app.use(passport.session());
 // Express View engine setup
 
 app.use(
-    require('node-sass-middleware')({
-        src: path.join(__dirname, 'public'),
-        dest: path.join(__dirname, 'public'),
-        sourceMap: true
-    })
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
 );
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-<<<<<<< HEAD
-app.locals.title = 'Welcome to Klipr';
-=======
-<<<<<<< HEAD
 app.locals.title = "Welcome to Klipr";
-=======
-app.locals.title = 'Express - Generated with IronGenerator';
->>>>>>> 3e2e3c357e58be21f701b6d535ac49e719b42656
->>>>>>> 23046b6f185f3449a7fdfd3ff0cb7b55b823ef99
 
-const index = require('./routes/index');
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,209 +1,216 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-const passport = require('passport');
-const ensureLogin = require('connect-ensure-login');
-const User = require('../models/User');
-const Article = require('../models/Article');
-const request = require('request');
-const cheerio = require('cheerio');
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+const User = require("../models/User");
+const Article = require("../models/Article");
+const request = require("request");
+const cheerio = require("cheerio");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-    res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 //LOGIN SECTION
 
 /* GET to ARRIVE at the LOGIN page */
-router.get('/login', (req, res, next) => {
-    res.render('login', { message: req.flash('error') });
+router.get("/login", (req, res, next) => {
+  res.render("login", { message: req.flash("error") });
 });
 
 //POST to SUBMIT once at the LOGIN page
 router.post(
-    '/login',
-    passport.authenticate('local', {
-        successRedirect: '/mylist',
-        failureRedirect: '/login',
-        failureFlash: true,
-        passReqToCallback: true,
-        badRequestMessage: 'Your message you want to change.'
-    })
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/mylist",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true,
+    badRequestMessage: "Your message you want to change."
+  })
 );
 
 //SIGNUP SECTION
 /* GET to ARRIVE at the SIGNUP page */
 
-router.get('/signup', (req, res, next) => {
-    res.render('signup');
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
 });
 
 //POST to SUBMIT once at the SIGNUP page
-router.post('/signup', (req, res, next) => {
-    //captures name, email and password from body
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
+router.post("/signup", (req, res, next) => {
+  //captures name, email and password from body
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    // if name, email or password is blank, then render error
-    if (name === '' || email === '' || password === '') {
-        res.render('signup', {
-            errorMessage: 'Please enter name, email and a password for signup'
-        });
-    }
+  // if name, email or password is blank, then render error
+  if (name === "" || email === "" || password === "") {
+    res.render("signup", {
+      errorMessage: "Please enter name, email and a password for signup"
+    });
+  }
 
-    //check is password length is greater than 8
-    if (password.length < 8) {
-        res.render('signup', {
-            errorMessage: 'Please create a password with 8 or more characters'
+  //check is password length is greater than 8
+  if (password.length < 8) {
+    res.render("signup", {
+      errorMessage: "Please create a password with 8 or more characters"
+    });
+    return;
+  }
+
+  //applies encryption (using salt method) to password - standard, don't change
+
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  //finally creates new user and add to Model/databse
+  //use Model.create()
+
+  //create new user object with entered name, email and encrypted password
+  const newUserObject = {
+    name: name,
+    email: email,
+    password: hashPass
+  };
+
+  // search if email already exists, else render error
+
+  User.findOne({ email: email })
+    .then(user => {
+      if (user !== null) {
+        res.render("signup", {
+          errorMessage: "The email already exists"
         });
         return;
-    }
+      }
 
-    //applies encryption (using salt method) to password - standard, don't change
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    //finally creates new user and add to Model/databse
-    //use Model.create()
-
-    //create new user object with entered name, email and encrypted password
-    const newUserObject = {
-        name: name,
-        email: email,
-        password: hashPass
-    };
-
-    // search if email already exists, else render error
-
-    User.findOne({ email: email })
-        .then(user => {
-            if (user !== null) {
-                res.render('signup', {
-                    errorMessage: 'The email already exists'
-                });
-                return;
-            }
-
-            // if email does not exist, create new Model
-            User.create(newUserObject)
-                .then(createdUser => {
-                    console.log(createdUser, 'User was successfully created');
-                    res.redirect('/mylist');
-                })
-                .catch(err => {
-                    console.log(err, 'User could not be created because this email already exists');
-                });
+      // if email does not exist, create new Model
+      User.create(newUserObject)
+        .then(createdUser => {
+          console.log(createdUser, "User was successfully created");
+          res.redirect("/mylist");
         })
         .catch(err => {
-            console.log(err, 'All fields are required.');
+          console.log(
+            err,
+            "User could not be created because this email already exists"
+          );
         });
+    })
+    .catch(err => {
+      console.log(err, "All fields are required.");
+    });
 });
 
 //LOGOUT SECTION
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/login');
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
 });
 
 //MYLIST SECTION
 
-router.get('/mylist', ensureLogin.ensureLoggedIn(), (req, res) => {
-    //check user's ID
-    let userID = req.user.id;
+router.get("/mylist", ensureLogin.ensureLoggedIn(), (req, res) => {
+  //check user's ID
+  let userID = req.user.id;
 
-    //display all articles
-    Article.find({ _owner: userID })
-        .then(articles => {
-            res.render('mylist', { user: req.user, articles });
-        })
-        .catch();
+  //display all articles
+  Article.find({ _owner: userID })
+    .then(articles => {
+      res.render("mylist", { user: req.user, articles });
+    })
+    .catch();
 });
 
 //SAVE ARTICLE SECTION
 
-router.post('/save', ensureLogin.ensureLoggedIn(), (req, res) => {
-    const url = req.body.url;
-    const userID = req.user.id;
+router.post("/save", ensureLogin.ensureLoggedIn(), (req, res) => {
+  const url = req.body.url;
+  const userID = req.user.id;
 
-    request(url, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            const $ = cheerio.load(body);
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const $ = cheerio.load(body);
 
-            //find the title in the head, take text, then trim
-            const title = $('head > title').text();
-            // .trim();
+      //find the title in the head, take text, then trim
+      const title = $("head > title").text();
+      // .trim();
 
-            //find the first paragraph then take the text
-            const description = $('p')
-                .first()
-                .text();
+      //find the first paragraph then take the text
+      const description = $("p")
+        .first()
+        .text();
 
-            //find a p greater than 100?
-            // $("p").filter(function() {
-            //   return $(this).text().length > 100;
-            // });
+      //find a p greater than 100?
+      // $("p").filter(function() {
+      //   return $(this).text().length > 100;
+      // });
 
-            const image = $('img')[0]['attribs']['src'];
+      const image = $("img")[0]["attribs"]["src"];
 
-            // create the newArticle Object
+      // create the newArticle Object
 
-            const newArticle = {
-                url: url,
-                title: title,
-                image: image,
-                description: description,
-                _owner: userID
-            };
+      const newArticle = {
+        url: url,
+        title: title,
+        image: image,
+        description: description,
+        _owner: userID
+      };
 
-            //console.log("This is the new article Object", newArticle);
+      //console.log("This is the new article Object", newArticle);
 
-            //Create new Article
+      //Create new Article
 
-            Article.create(newArticle)
-                .then(createdArticle => {
-                    console.log(createdArticle, 'Article successfully created');
-                    res.redirect('/mylist');
-                })
-                .catch(err => {
-                    console.log(err, 'Sorry, article was not created!');
-                });
-        }
-    });
+      Article.create(newArticle)
+        .then(createdArticle => {
+          console.log(createdArticle, "Article successfully created");
+          res.redirect("/mylist");
+        })
+        .catch(err => {
+          console.log(err, "Sorry, article was not created!");
+        });
+    }
+  });
 });
 
 //DELETE ARTICLE SECTION
 
-router.post('/mylist/:id/delete', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+router.post(
+  "/mylist/:id/delete",
+  ensureLogin.ensureLoggedIn(),
+  (req, res, next) => {
     const id = req.params.id;
     Article.findByIdAndRemove(id)
-        .then(_ => {
-            console.log('Article was DELETED!');
-            res.redirect('/mylist');
-        })
-        .catch(err => {
-            console.log(err, 'Article was NOT deleted.');
-        });
-});
+      .then(_ => {
+        console.log("Article was DELETED!");
+        res.redirect("/mylist");
+      })
+      .catch(err => {
+        console.log(err, "Article was NOT deleted.");
+      });
+  }
+);
 
 //FAVORITES SECTION
-router.get('/favorites', (req, res, next) => {
-    res.render('favorites');
+router.get("/favorites", (req, res, next) => {
+  res.render("favorites");
 });
 
 //ABOUT SECTION
-router.get('/about', (req, res, next) => {
-    res.render('about');
+router.get("/about", (req, res, next) => {
+  res.render("about");
 });
 
 //CAREERS SECTION
 
-router.get('/careers', (req, res, next) => {
-    res.render('careers');
+router.get("/careers", (req, res, next) => {
+  res.render("careers");
 });
 
 module.exports = router;

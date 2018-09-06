2f4a9301ec5f843cd60f400d9791df425ea8b072
kaschief <kaschiefj@gmail.com>
2018-09-06 16:16:23 +0200

    favicon, search button

--- app.js
+++ app.js
@@ -109,9 +109,9 @@ app.use(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 app.use((req, res, next) => {
   res.locals.isConnected = req.isAuthenticated();
+  // res.locals.currentUser = res.user;
   next();
 });
 
